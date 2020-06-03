5832747b6f14d80bc1d88ab70045cb504d19bf01
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-03 14:11:24 -0500

    working on MVP

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const Postsdb = require('./postDb')
+
 router.get('/', (req, res) => {
-  // do your magic!
+
+  Postsdb.get(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the posts',
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express');
 
 const server = express();
 
+const postRouter = require('./posts/postRouter');
+
+const logger = require('morgan')
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+// function logger(req, res, next) {
+
+
+// }
+
+
+
+
+
+server.use(logger(':method :url :date'));
+server.use(express.json());
+server.use('/api/posts', postRouter);
 
 module.exports = server;
